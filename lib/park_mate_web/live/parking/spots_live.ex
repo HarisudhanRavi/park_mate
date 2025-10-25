d468654da3ddd4b1216e4fbb257f566f4f2c31f2
Harisudhan V R <harisudhan.v.r@maersk.com>
2025-10-25 23:44:10 +0530

    Render spots in live

--- lib/park_mate_web/live/parking/spots_live.ex
+++ lib/park_mate_web/live/parking/spots_live.ex
@@ -1,15 +1,40 @@
 defmodule ParkMateWeb.Parking.SpotsLive do
   use ParkMateWeb, :live_view
+  alias ParkMate.Parking.Manager
 
-  def mount(%{"floor" => _floor}, _session, socket) do
-    {:ok, socket}
+  def mount(%{"floor" => floor}, _session, socket) do
+    floor = String.to_existing_atom(floor)
+
+    spots = Manager.get_all_spaces() |> Map.get(floor) |> convert_status_to_color()
+
+    {:ok, socket |> assign(:floor, floor) |> assign(:spots, spots)}
   end
 
   def render(assigns) do
     ~H"""
     <div>
       <h1 class="text-2xl font-extrabold dark:text-white mb-4">Parking</h1>
+
+      <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Select a spot to park
+        </h5>
+        <%= for {_spot_name, spot_details} <- @spots do %>
+          <span phx-click="" class="cursor-pointer mx-4">
+            <.icon name="hero-square-2-stack-solid" class={spot_details.status} />
+          </span>
+        <% end %>
+      </div>
     </div>
     """
   end
+
+  defp convert_status_to_color(spots) do
+    Enum.map(spots, fn {spot_name, details} ->
+      {spot_name, %{details | status: status_color(details.status)}}
+    end)
+  end
+
+  defp status_color(:free), do: "text-green-500"
+  defp status_color(:occupied), do: "text-red-500"
 end
